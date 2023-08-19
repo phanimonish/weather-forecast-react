09a8f5e3902c0f3081b0725cbbab349dbaa9f285
phanimonish <phanimonish9261@gmail.com>
2023-08-19 11:48:19 +0530

    1st commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ce333f3d26msh5b59fbc182113f5p1e7c16jsn2b5e38a93c80',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "8a58c0a7110942e355d570c098763b48";
